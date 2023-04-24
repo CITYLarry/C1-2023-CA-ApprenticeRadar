7168bb1f310c4d7c4798ac3fd0b842bb416b0c2d
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 08:39:15 -0500

    initial commit

---
+++ src/main/java/co/com/sofka/apprenticeradar/ApprenticeRadarApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.apprenticeradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprenticeRadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofka/apprenticeradar/ApprenticeRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.apprenticeradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

bc1f673541450df6a46c58dd236b2ca4f1ba15d1
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 09:59:08 -0500

    Create the Radar model

---
+++ src/main/java/co/com/sofka/apprenticeradar/domain/model/Radar.java
@@ -0,0 +1,33 @@
+package co.com.sofka.apprenticeradar.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.stream.Stream;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Radar {
+
+    private String knowledgeArea;
+    private String description;
+
+    private Double factual;
+    private Double conceptual;
+    private Double procedural;
+    private Double metacognitive;
+    private Double approvalLevel = Stream.of(factual, conceptual, procedural, metacognitive)
+            .mapToDouble(Double::doubleValue)
+            .average()
+            .orElse(Double.NaN);
+
+
+
+
+}

1278b3e24bf11d35cd2a3e9d47b787611254ed58
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 10:21:24 -0500

    Add the missing parameter radarId

--- src/main/java/co/com/sofka/apprenticeradar/domain/model/Radar.java
+++ src/main/java/co/com/sofka/apprenticeradar/domain/model/Radar.java
@@ -15,6 +15,7 @@ import java.util.stream.Stream;
 @Builder(toBuilder = true)
 public class Radar {
 
+    private String radarId;
     private String knowledgeArea;
     private String description;
 

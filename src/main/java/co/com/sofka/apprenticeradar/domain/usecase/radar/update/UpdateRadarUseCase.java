307d61fc5f09b149dae63f895919019f8732ca99
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 14:12:17 -0500

    Set the final statement on the parameter definition

--- src/main/java/co/com/sofka/apprenticeradar/domain/usecase/radar/update/UpdateRadarUseCase.java
+++ src/main/java/co/com/sofka/apprenticeradar/domain/usecase/radar/update/UpdateRadarUseCase.java
@@ -10,7 +10,7 @@ import java.util.function.BiFunction;
 @RequiredArgsConstructor
 public class UpdateRadarUseCase implements BiFunction<String, Radar, Mono<Radar>> {
 
-    private RadarGateway gateway;
+    private final RadarGateway gateway;
 
     public Mono<Radar> apply(String radarId, Radar radar) {
         return gateway.updateRadar(radarId, radar);

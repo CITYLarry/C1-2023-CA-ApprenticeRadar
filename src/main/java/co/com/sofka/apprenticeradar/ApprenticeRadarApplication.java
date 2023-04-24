699fe4f9fbdebded0d66c011aec9f4926a690f78
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 10:47:12 -0500

    Add main documentation

--- src/main/java/co/com/sofka/apprenticeradar/ApprenticeRadarApplication.java
+++ src/main/java/co/com/sofka/apprenticeradar/ApprenticeRadarApplication.java
@@ -1,9 +1,17 @@
 package co.com.sofka.apprenticeradar;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Apprentice Radar API",
+        version = "1.0.0",
+        description = "API created for the agile challenge at SofkaU Bilingual Training League"
+))
+
 public class ApprenticeRadarApplication {
 
     public static void main(String[] args) {

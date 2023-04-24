4f4f5ae0280907d9c895bfd9d4f0dbd7a607dd75
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-04-24 10:22:12 -0500

    Create the persistence entity

---
+++ src/main/java/co/com/sofka/apprenticeradar/infrastructure/drivenadapters/mongo/radar/data/RadarData.java
@@ -0,0 +1,28 @@
+package co.com.sofka.apprenticeradar.infrastructure.drivenadapters.mongo.radar.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.stream.Stream;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "radars")
+public class RadarData {
+
+    @Id
+    private String radarId;
+    private String knowledgeArea;
+    private String description;
+
+    private Double factual;
+    private Double conceptual;
+    private Double procedural;
+    private Double metacognitive;
+    private Double approvalLevel = Stream.of(factual, conceptual, procedural, metacognitive)
+            .mapToDouble(Double::doubleValue)
+            .average()
+            .orElse(Double.NaN);
+}
